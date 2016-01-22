c9c540f9e7d1de3ec4439a6b6958fc1c6fa16b8e
Arnault <arnault@lal.in2p3.fr>
2016-01-22 09:45:35 +0100

    fix wcs algo

--- twcs.py
+++ twcs.py
@@ -5,77 +5,93 @@
 
 from __future__ import division # confidence high
 
-import numpy
-import pywcs
-import pyfits
+import numpy as np
+from astropy import wcs as pywcs
+from astropy.io import fits
 import re
 
-def xy_to_radec(header, X, Y):
-    x0 = header['CRPIX1']
-    y0 = header['CRPIX2']
+class WCS:
+    def __init__(self, header):
+        self.xy0 = np.array([header['CRPIX1'], header['CRPIX2']])
 
-    a = header['CD1_1']
-    b = header['CD1_2']
-    c = header['CRVAL1']
-    d = header['CD2_1']
-    e = header['CD2_2']
-    f = header['CRVAL2']
+        a = header['CD1_1']
+        b = header['CD1_2']
+        c = header['CRVAL1']
+        d = header['CD2_1']
+        e = header['CD2_2']
+        f = header['CRVAL2']
 
-    x = X - x0
-    y = Y - y0
+        self.matrix = np.array([[a, b], [d, e]])
+        self.rd0 = np.array([c, f])
 
-    ra = (a*x + b*y) + c
-    dec = (d*x + e*y) + f
+    def xy_to_radec(self, xy):
+        xy -= self.xy0
+        rd = self.matrix.dot(xy) + self.rd0
+        return rd
 
-    return ra, dec
+    def radec_to_xy(self, rd):
+        tmat = self.matrix.conj()
+        rd -= self.rd0
+        xy = tmat.dot(rd) + self.xy0
 
-def radec_to_xy(header, ra, dec):
-    x0 = header['CRPIX1']
-    y0 = header['CRPIX2']
+        return xy
 
-    a = header['CD1_1']
-    b = header['CD1_2']
-    c = header['CRVAL1']
-    d = header['CD2_1']
-    e = header['CD2_2']
-    f = header['CRVAL2']
 
-    numerator = b*d - a*e
+def read_hdus(fitsfile):
+    """
+    Get pixels from FITS file
 
-    x = (b*f - e*c) / numerator
-    y = (d*c - a*f) / numerator
+    cf http://stsdas.stsci.edu/stsci_python_epydoc/pyfits/api_hdulists.html
 
-    x += x0
-    y += y0
-
-    return x, y
+    Return a HDUList()
+    """
+    hdus = None
+    try:
+        with fits.open(fitsfile) as data_fits:
+            try:
+                data_fits.verify('silentfix')
+                return data_fits
+            except ValueError as err:
+                logging.error('Error: %s', err)
+    except EnvironmentError as err:
+        logging.error('Cannot open the data fits file. - %s', err)
+    return hdus
 
 # Load the FITS hdulist using pyfits
-hdulist = pyfits.open('data/03BL01/D3/2004-01-13/i/732183p.fits.fz')
+hdus = read_hdus('data/03BL01/D3/2004-01-13/i/732183p.fits.fz')
 
 # Parse the WCS keywords in the primary HDU
 
 for n in range(1, 36):
-    header = hdulist[n].header
+    print '--------------------------------'
+    header = hdus[n].header
     wcs = pywcs.WCS(header)
 
-    # Print out the "name" of the WCS, as defined in the FITS header
-    # print wcs.wcs.name
-
-    # Print out all of the settings that were parsed from the header
-    # print '---------------------'
-    # print wcs.wcs
-    # print '---------------------'
+    mywcs = WCS(header)
 
     # Some pixel coordinates of interest.
-    detsize = header['DETSIZE']
+    detsize = header['DATASEC']
     m = re.match('[^\[]*\[([0-9]+):([0-9]+),([0-9]+):([0-9]+).*', detsize)
     low1 = int(m.group(1))
     high1 = int(m.group(2))
     low2 = int(m.group(3))
     high2 = int(m.group(4))
 
-    ra1, dec1 = xy_to_radec(header, low1, low2)
+    low = [low1, low2]
+    high = [high1, high2]
 
-    ra2, dec2 = xy_to_radec(header, high1, high2)
-    print detsize, ra1, dec1, ra2, dec2
+    pixel = np.array([low, high], np.float_)
+    sky = wcs.wcs_pix2world(pixel, 0)
+
+    rd_low = mywcs.xy_to_radec(low)
+    rd_high = mywcs.xy_to_radec(high)
+
+    # print low, high
+
+    print 'mat=', rd_low, rd_high
+
+    d_low  = sky[0] - rd_low
+    d_high = sky[1] - rd_high
+
+    print 'd  =', d_low, d_high
+

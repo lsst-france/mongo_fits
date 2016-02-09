30f5352c1bd8610b6ea4f30b31148440bc061c5f
Arnault <arnault@lal.in2p3.fr>
2016-02-09 18:37:43 +0100

    fix wcs algo

--- twcs.py
+++ twcs.py
@@ -11,35 +11,73 @@ from astropy.io import fits
 import re
 
 class WCS:
+    """
+    Class to implement the transformation algorithm to convert pixel coordinate into WCS coordinates
+    this reflects what is provided in the wcslib module
+    """
     def __init__(self, header):
-        self.xy0 = np.array([header['CRPIX1'], header['CRPIX2']])
+        """
+        Constructor: fetch all WCS information to setup parameters needed for the
+        conversion algorithm
 
-        a = header['CD1_1']
-        b = header['CD1_2']
-        c = header['CRVAL1']
-        d = header['CD2_1']
-        e = header['CD2_2']
-        f = header['CRVAL2']
+        :param header: this is the FITS header
+        :return: Nonz
+        """
 
-        self.matrix = np.array([[a, b], [d, e]])
-        self.rd0 = np.array([c, f])
+        CRPIX1 = header['CRPIX1']
+        CRPIX2 = header['CRPIX2']
+
+        CD1_1  = header['CD1_1']
+        CD1_2  = header['CD1_2']
+        CRVAL1 = header['CRVAL1']
+        CD2_1  = header['CD2_1']
+        CD2_2  = header['CD2_2']
+        CRVAL2 = header['CRVAL2']
+
+        # prepare the transfromation matrix
+
+        self.xy0 = np.array([CRPIX1, CRPIX2], np.float64)
+
+        a = CD1_1
+        b = CD1_2
+        c = CRVAL1
+        d = CD2_1
+        e = CD2_2
+        f = CRVAL2
+
+        self.matrix = np.array([[a, b], [d, e]], np.float64)
+        self.ra_dec0 = np.array([c, f], np.float64)
+
 
     def xy_to_radec(self, xy):
+        """
+        Function to convert a pixel coordinates into a WCS coordonates
+
+        :param xy: a 1D ndarray
+        :return: ra_dec the result of the conversion as a 1d-array
+        """
+
         xy -= self.xy0
-        rd = self.matrix.dot(xy) + self.rd0
-        return rd
 
-    def radec_to_xy(self, rd):
-        tmat = self.matrix.conj()
-        rd -= self.rd0
-        xy = tmat.dot(rd) + self.xy0
+        ra_dec = self.matrix.dot(xy)
 
-        return xy
+        dec = self.ra_dec0[1] + ra_dec[1]
+        dec_radians = 2.0 * np.pi * dec/360.0
+
+        scale = np.array([np.cos(dec_radians), 1.0])
+
+        ra_dec /= scale
+        ra_dec += self.ra_dec0
+
+        return ra_dec
+
+    def radec_to_xy(self, ra_dec):
+        # not yet done
+        return None
 
 
 def read_hdus(fitsfile):
-    """
-    Get pixels from FITS file
+    """ pixels from FITS file
 
     cf http://stsdas.stsci.edu/stsci_python_epydoc/pyfits/api_hdulists.html
 
@@ -52,9 +90,9 @@ def read_hdus(fitsfile):
                 data_fits.verify('silentfix')
                 return data_fits
             except ValueError as err:
-                logging.error('Error: %s', err)
+                print 'Error: %s' % err
     except EnvironmentError as err:
-        logging.error('Cannot open the data fits file. - %s', err)
+        print 'Cannot open the data fits file. - %s' % err
     return hdus
 
 # Load the FITS hdulist using pyfits
@@ -72,26 +110,28 @@ for n in range(1, 36):
     # Some pixel coordinates of interest.
     detsize = header['DATASEC']
     m = re.match('[^\[]*\[([0-9]+):([0-9]+),([0-9]+):([0-9]+).*', detsize)
-    low1 = int(m.group(1))
-    high1 = int(m.group(2))
-    low2 = int(m.group(3))
-    high2 = int(m.group(4))
+    low1 = np.float64(m.group(1))
+    high1 = np.float64(m.group(2))
+    low2 = np.float64(m.group(3))
+    high2 = np.float64(m.group(4))
 
     low = [low1, low2]
     high = [high1, high2]
 
-    pixel = np.array([low, high], np.float_)
+    pixel = np.array([low, high], np.float64)
     sky = wcs.wcs_pix2world(pixel, 0)
 
-    rd_low = mywcs.xy_to_radec(low)
-    rd_high = mywcs.xy_to_radec(high)
+    ra_dec_low = mywcs.xy_to_radec(low)
+    ra_dec_high = mywcs.xy_to_radec(high)
 
-    # print low, high
+    # print 'low=', low, 'high=', high
 
-    print 'mat=', rd_low, rd_high
+    print 'mat=', ra_dec_low, ra_dec_high
+    print 'wcs=', sky[0], sky[1]
 
-    d_low  = sky[0] - rd_low
-    d_high = sky[1] - rd_high
+    d_low  = (sky[0] - ra_dec_low)/sky[0]
+    d_high = (sky[1] - ra_dec_high)/sky[1]
 
     print 'd  =', d_low, d_high
+
 
